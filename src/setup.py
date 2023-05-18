a736504eea3ab027a9832b32adf45677de44a5b7
dserez <serez1997@gmail.com>
2023-05-18 09:54:02 +0200

    update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
-from pathlib import Path
 
-# set readme
-this_directory = Path(__file__).parent
-long_description = (this_directory / "../readme.md").read_text()
 
 setup(name='ffcv_pl',
-      version='0.2.0',
+      version='0.2.1',
       description='manage fast data loading with ffcv and pytorch lightning',
-      long_description=long_description,
+      long_description='see [github project](https://github.com/SerezD/ffcv_pytorch_lightning) ',
       long_description_content_type='text/markdown',
       url='https://github.com/SerezD/ffcv_pytorch_lightning',
       author='DSerez',
