bb217cfbab5a83930f24fa3a7b06812a2d0a6ed6
dserez <serez1997@gmail.com>
2023-07-13 13:36:52 +0200

    updated version 0.3.1

--- src/setup.py
+++ src/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 
 setup(name='ffcv_pl',
-      version='0.3.0',
-      packages=['ffcv_pl'],
+      version='0.3.1',
+      packages=['ffcv_pl', 'ffcv_pl.ffcv_utils'],
       description='manage fast data loading with ffcv and pytorch lightning',
       long_description='see [github project](https://github.com/SerezD/ffcv_pytorch_lightning) ',
       long_description_content_type='text/markdown',
