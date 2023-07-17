9ac31fcb2a102d6bb7d79c1d6ae6354ead3a8432
dserez <serez1997@gmail.com>
2023-07-17 12:10:40 +0200

    data loading params

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+long_description = """
+                   Version Updates: allow optional arguments for FFCVDataModule and create_beton_wrapper.\n
+                   For example usages/installation, 
+                   check [github project](https://github.com/SerezD/ffcv_pytorch_lightning)
+                   """
 
 setup(name='ffcv_pl',
-      version='0.3.1',
+      version='0.3.2',
       packages=['ffcv_pl', 'ffcv_pl.ffcv_utils'],
       description='manage fast data loading with ffcv and pytorch lightning',
-      long_description='see [github project](https://github.com/SerezD/ffcv_pytorch_lightning) ',
+      long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/SerezD/ffcv_pytorch_lightning',
       author='DSerez',
